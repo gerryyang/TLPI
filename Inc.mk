876c94692c57cf8327a7d1715c90c8eeb5c5402c
gerryyang <diggwcdj@gmail.com>
2014-05-25 16:42:40 +0800

    add fileio src

--- Inc.mk
+++ Inc.mk
@@ -8,8 +8,8 @@ AROPT = -scurv
 
 CFLAGS = -Werror -g -Os -pipe -D_REENTRANT
 
-INCLUDE = -I../tlpi
-LIBPATH = -L../lib
+INCLUDE = -I/Users/gerryyang/github_project/TLPI/tlpi
+LIBPATH = -L/Users/gerryyang/github_project/TLPI/lib
 
 DEP_LIBS = -ltlpi
 LIBS = $(DEP_LIBS)
