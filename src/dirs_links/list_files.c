3dc5912e28a02543c191678b1bb727a2a5be29c5
gerryyang <diggwcdj@gmail.com>
2014-08-06 09:06:40 +0800

    add list files

---
+++ src/dirs_links/list_files.c
@@ -0,0 +1,97 @@
+/*************************************************************************\
+ *                  Copyright (C) Michael Kerrisk, 2014.                   *
+ *                                                                         *
+ * This program is free software. You may use, modify, and redistribute it *
+ * under the terms of the GNU Affero General Public License as published   *
+ * by the Free Software Foundation, either version 3 or (at your option)   *
+ * any later version. This program is distributed without any warranty.    *
+ * See the file COPYING.agpl-v3 for details.                               *
+ \*************************************************************************/
+
+/* list_files.c
+
+   Demonstrate the use of opendir() and related functions to list files
+   in a directory.
+
+   Walk through each directory named on the command line (current directory
+   if none are specified) to display a list of the files it contains.
+
+Usage: list_files [dir...]
+*/
+#if defined(__APPLE__)
+/* Darwin requires this header before including <dirent.h> */
+#include <sys/types.h>
+#endif
+#include <dirent.h>
+#include "tlpi_hdr.h"
+
+static void             /* List all files in directory 'dirpath' */
+listFiles(const char *dirpath)
+{
+	DIR *dirp;
+	struct dirent *dp;
+	Boolean isCurrent;          /* True if 'dirpath' is "." */
+
+	isCurrent = strcmp(dirpath, ".") == 0;
+
+	dirp = opendir(dirpath);
+	if (dirp  == NULL) {
+		errMsg("opendir failed on '%s'", dirpath);
+		return;
+	}
+
+	/* For each entry in this directory, print directory + filename */
+
+	for (;;) {
+		errno = 0;              /* To distinguish error from end-of-directory */
+		dp = readdir(dirp);
+		if (dp == NULL)
+			break;
+
+		if (strcmp(dp->d_name, ".") == 0 || strcmp(dp->d_name, "..") == 0)
+			continue;           /* Skip . and .. */
+
+		if (!isCurrent)
+			printf("%s/", dirpath);
+		printf("%s\n", dp->d_name);
+	}
+
+	if (errno != 0)
+		errExit("readdir");
+
+	if (closedir(dirp) == -1)
+		errMsg("closedir");
+}
+
+int
+main(int argc, char *argv[])
+{
+	if (argc > 1 && strcmp(argv[1], "--help") == 0)
+		usageErr("%s [dir-path...]\n", argv[0]);
+
+	if (argc == 1)              /* No arguments - use current directory */
+		listFiles(".");
+	else
+		for (argv++; *argv; argv++)
+			listFiles(*argv);
+
+	exit(EXIT_SUCCESS);
+}
+/*
+output:
+gerryyang@mba:dirs_links$ls
+Makefile     list_files.c  t_unlink*   t_unlink.o     view_symlink.c
+list_files*  list_files.o  t_unlink.c  view_symlink*  view_symlink.o
+gerryyang@mba:dirs_links$./list_files 
+list_files
+list_files.c
+list_files.o
+Makefile
+t_unlink
+t_unlink.c
+t_unlink.o
+view_symlink
+view_symlink.c
+view_symlink.o
+ */
+
