b78734ffdce928377dd5ad2558848870f761fffc
gerryyang <diggwcdj@gmail.com>
2014-10-12 22:46:10 +0800

    add a simple POSIX threads producer-consumer example that doesn't use a condition variable

---
+++ src/threads/prod_no_condvar.c
@@ -0,0 +1,143 @@
+/*************************************************************************\
+ *                  Copyright (C) Michael Kerrisk, 2014.                   *
+ *                                                                         *
+ * This program is free software. You may use, modify, and redistribute it *
+ * under the terms of the GNU Affero General Public License as published   *
+ * by the Free Software Foundation, either version 3 or (at your option)   *
+ * any later version. This program is distributed without any warranty.    *
+ * See the file COPYING.agpl-v3 for details.                               *
+ \*************************************************************************/
+
+/* prod_no_condvar.c
+
+   A simple POSIX threads producer-consumer example that doesn't use
+   a condition variable.
+
+   See also prod_condvar.c.
+   */
+#include <time.h>
+#include <pthread.h>
+#include "tlpi_hdr.h"
+
+static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
+
+static int avail = 0;
+
+static void *
+threadFunc(void *arg)
+{
+	int cnt = atoi((char *) arg);
+	int s, j;
+
+	for (j = 0; j < cnt; j++) {
+		sleep(1);
+
+		/* Code to produce a unit omitted */
+
+		s = pthread_mutex_lock(&mtx);
+		if (s != 0)
+			errExitEN(s, "pthread_mutex_lock");
+
+		avail++;        /* Let consumer know another unit is available */
+
+		s = pthread_mutex_unlock(&mtx);
+		if (s != 0)
+			errExitEN(s, "pthread_mutex_unlock");
+	}
+
+	return NULL;
+}
+
+int
+main(int argc, char *argv[])
+{
+	pthread_t tid;
+	int s, j;
+	int totRequired;            /* Total number of units that all
+								   threads will produce */
+	int numConsumed;            /* Total units so far consumed */
+	Boolean done;
+	time_t t;
+
+	if (argc < 2)
+	{
+		printf("usage: %s jobs\n", argv[0]);
+		return 0;
+	}
+
+	t = time(NULL);
+
+	/* Create all threads */
+
+	totRequired = 0;
+	for (j = 1; j < argc; j++) {
+		totRequired += atoi(argv[j]);
+
+		s = pthread_create(&tid, NULL, threadFunc, argv[j]);
+		if (s != 0)
+			errExitEN(s, "pthread_create");
+	}
+
+	/* Use a polling loop to check for available units */
+
+	numConsumed = 0;
+	done = FALSE;
+
+	for (;;) {
+		s = pthread_mutex_lock(&mtx);
+		if (s != 0)
+			errExitEN(s, "pthread_mutex_lock");
+
+		while (avail > 0) {             /* Consume all available units */
+
+			/* Do something with produced unit */
+
+			numConsumed ++;
+			avail--;
+			printf("T=%ld: numConsumed=%d\n", (long) (time(NULL) - t),
+					numConsumed);
+
+			done = numConsumed >= totRequired;
+		}
+
+		s = pthread_mutex_unlock(&mtx);
+		if (s != 0)
+			errExitEN(s, "pthread_mutex_unlock");
+
+		if (done)
+			break;
+
+		/* Perhaps do other work here that does not require mutex lock */
+
+	}
+
+	exit(EXIT_SUCCESS);
+}
+/*
+output:
+gerryyang@mba:threads$./prod_no_condvar 2
+T=1: numConsumed=1
+T=2: numConsumed=2
+gerryyang@mba:threads$./prod_no_condvar 2 3
+T=1: numConsumed=1
+T=1: numConsumed=2
+T=2: numConsumed=3
+T=2: numConsumed=4
+T=3: numConsumed=5
+gerryyang@mba:threads$./prod_no_condvar 5 5 5
+T=1: numConsumed=1
+T=1: numConsumed=2
+T=1: numConsumed=3
+T=2: numConsumed=4
+T=2: numConsumed=5
+T=2: numConsumed=6
+T=3: numConsumed=7
+T=3: numConsumed=8
+T=3: numConsumed=9
+T=4: numConsumed=10
+T=4: numConsumed=11
+T=4: numConsumed=12
+T=5: numConsumed=13
+T=5: numConsumed=14
+T=5: numConsumed=15
+ */
